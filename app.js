1793345eb803e75dd8a9999b96209c79a63bb025
Mohamed Kalanther <ksmk@meerana.ae>
2024-11-06 17:15:46 +0530

    group chat fix

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'developments' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
@@ -39,3 +39,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+//...............................
